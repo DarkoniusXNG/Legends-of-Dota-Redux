75ccc0aa0516492417226ad78a3066256a4a1183
Darko V <darko1290@gmail.com>
2025-02-11 00:22:09 +0100

    new Death Prophet perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_death_prophet_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_death_prophet_perk.lua
@@ -1,6 +1,6 @@
 --------------------------------------------------------------------------------------------------------
 --    Hero: Death Prophet
---    Perk: 
+--    Perk: Death Prophet gains +1% Spell Amp for each level put in a Undead ability.
 --------------------------------------------------------------------------------------------------------
 modifier_npc_dota_hero_death_prophet_perk = modifier_npc_dota_hero_death_prophet_perk or class({})
 --------------------------------------------------------------------------------------------------------
@@ -22,4 +22,35 @@ end
 
 function modifier_npc_dota_hero_death_prophet_perk:GetTexture()
 	return "custom/npc_dota_hero_death_prophet_perk"
+end
+
+function modifier_npc_dota_hero_death_prophet_perk:OnCreated()
+	self.bonusPerLevel = 1
+	if IsServer() then
+		self:StartIntervalThink(0.1)
+	end
+end
+
+function modifier_npc_dota_hero_death_prophet_perk:OnIntervalThink()
+	if IsServer() then
+		local caster = self:GetParent()
+		local stacks = 0
+		for i = 0, caster:GetAbilityCount() - 1 do
+			local skill = caster:GetAbilityByIndex(i)
+			if skill and skill:HasAbilityFlag("undead") then
+				stacks = stacks + skill:GetLevel() * self.bonusPerLevel
+			end
+		end
+		self:SetStackCount(stacks)
+	end
+end
+
+function modifier_npc_dota_hero_death_prophet_perk:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
+	}
+end
+
+function modifier_npc_dota_hero_death_prophet_perk:GetModifierSpellAmplify_Percentage()
+	return self:GetStackCount()
 end
