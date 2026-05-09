4dbc99f8a0f9d84f1c8d7e3b5b52929c3132090e
Darko V <darko1290@gmail.com>
2026-05-09 22:38:04 +0200

    Voidal Flare
    Voidal Flare cast range reduced from 900/1000/1100/1200 to 750.
    Voidal Flare cd rescaled from 5.5/4.5/3.5/2.5 to 5/4.5/4/3.5 seconds.
    Voidal Flare initial dmg increased from 70/100/130/160 to 80/120/160/200.
    Voidal Flare dmg per stack reduced from 30/50/70/90 to 30/40/50/60.
    Voidal Flare stack duration increased from 60s to 90s.
    Voidal Flare: Added sounds.
    Voidal Flare: Fixed projectile not being disjointable.
    Voidal Flare projectile speed reduced from 1700 to 1400.
    Voidal Flare: Fixed projectile ignoring spell block, spell reflect and invulnerability.
    Voidal Flare: Fixed voidal flare stacks being dispellable.
    Voidal Flare: Fixed projectile sometimes not being visible to enemies.

--- src/game/scripts/vscripts/abilities/dusk/gemini_voidal_flare.lua
+++ src/game/scripts/vscripts/abilities/dusk/gemini_voidal_flare.lua
@@ -1,7 +1,3 @@
--- Fires a projectile at the target enemy. When it hits them, it annihilates all magical impurities,
--- stunning and dealing Pure damage.
--- The damage and stun duration doubles for each time it's used on any target.
-
 gemini_voidal_flare = class({})
 
 LinkLuaModifier("modifier_voidal_flare","abilities/dusk/gemini_voidal_flare",LUA_MODIFIER_MOTION_NONE)
@@ -10,65 +6,70 @@ function gemini_voidal_flare:OnSpellStart()
 	local c = self:GetCaster()
 	local t = self:GetCursorTarget()
 
-	local spawn_origin = c:GetAttachmentOrigin(DOTA_PROJECTILE_ATTACHMENT_ATTACK_1)
-
 	c:EmitSound("Voidwalker.VoidalFlare")
 
-	local info = 
-	  {
-	  Target = t,
-	  Source = c,
-	  Ability = self,  
-	  EffectName = "particles/units/heroes/hero_gemini/voidal_flare.vpcf",
-	  vSpawnOrigin = spawn_origin,
-	  fDistance = 10000,
-	  fStartRadius = 32,
-	  fEndRadius = 32,
-	  bHasFrontalCone = false,
-	  bReplaceExisting = false,
-	  iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
-	  iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE,
-	  iUnitTargetType = DOTA_UNIT_TARGET_HERO+DOTA_UNIT_TARGET_BASIC,
-	  fExpireTime = GameRules:GetGameTime() + 10.0,
-	  bDeleteOnHit = true,
-	  iMoveSpeed = 1700,
-	  bProvidesVision = false,
-	  iVisionRadius = 0,
-	  iVisionTeamNumber = c:GetTeamNumber(),
-	  iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
-	  }
-  
-  	local projectile = ProjectileManager:CreateTrackingProjectile(info)
+	local info = {
+		EffectName = "particles/units/heroes/hero_gemini/voidal_flare.vpcf", -- essential if you want to see the projectile
+		Ability = self, -- essential if you want OnProjectileHit to work
+		Source = c, -- essential
+		Target = t, -- essential if you want it to travel towards the target
+		bProvidesVision = false, -- essential
+		iVisionRadius = 0, -- essential
+		iVisionTeamNumber = c:GetTeamNumber(), -- essential
+		iMoveSpeed = self:GetSpecialValueFor("projectile_speed"), -- essential because it is projectile speed
+		fExpireTime = GameRules:GetGameTime() + 10.0,
+		bDodgeable = true, -- essential if you want the projectile to be disjointable
+		--bIsAttack = false,
+		bReplaceExisting = false,
+		--bIgnoreObstructions = false,
+		--bSuppressTargetCheck = false,
+		iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1, -- DOTA_PROJECTILE_ATTACHMENT_ATTACK_2
+		--bDrawsOnMinimap = false,
+		bVisibleToEnemies = true, -- essential if you want to avoid visual bugs
+	}
+
+	ProjectileManager:CreateTrackingProjectile(info)
 end
 
 function gemini_voidal_flare:OnProjectileHit(t,l)
-	if t then
+	local caster = self:GetCaster()
+	if t and not t:IsNull() then
 		local duration = self:GetSpecialValueFor("duration")
 		local modifier = t:FindModifierByName("modifier_voidal_flare")
 
-		if t:IsMagicImmune() then return end
+		if t:IsMagicImmune() or t:IsInvulnerable() or t:TriggerSpellAbsorb( self ) then return end
 
 		local stack = 1
 
-		t:AddNewModifier(self:GetCaster(), self, "modifier_voidal_flare", {Duration=duration, stack=1})
+		t:AddNewModifier(caster, self, "modifier_voidal_flare", {duration = duration, stack = 1})
 		-- will set the stack to 1 if creating, or add when refreshing
 
 		if modifier then
 			stack = modifier:GetStackCount()
 		end
 
-		local damage_bonus = self:GetSpecialValueFor("damage_bonus")
-		local damage = self:GetSpecialValueFor("damage") + damage_bonus * (stack-1)
+		local damage = self:GetSpecialValueFor("damage") + self:GetSpecialValueFor("damage_bonus") * (stack-1)
 		local stun = self:GetSpecialValueFor("stun") + self:GetSpecialValueFor("stun_bonus") * (stack-1)
 
-		InflictDamage(t,self:GetCaster(),self,damage,DAMAGE_TYPE_MAGICAL)
+		InflictDamage(t,caster,self,damage,DAMAGE_TYPE_MAGICAL)
 
-		t:AddNewModifier(self:GetCaster(), self, "modifier_stunned", {Duration=stun})
-		t:EmitSound("Voidwalker.VoidalFlare.Hit")
+		t:AddNewModifier(caster, self, "modifier_stunned", {duration = stun})
+		t:EmitSound("Voidwalker.VoidalFlareHit")
 	end
+	return true
 end
 
+---------------------------------------------------------------------------------------------------
+
 modifier_voidal_flare = class({})
+
+function modifier_voidal_flare:IsDebuff()
+	return true
+end
+
+function modifier_voidal_flare:IsPurgable()
+	return false
+end
 
 function modifier_voidal_flare:OnCreated(kv)
 	if IsServer() then
@@ -89,6 +90,8 @@ function modifier_voidal_flare:OnRefresh(kv)
 		self:SetStackCount(self:GetStackCount()+kv.stack)
 	end
 end
+
+---------------------------------------------------------------------------------------------------
 
 function InflictDamage(target,attacker,ability,damage,damage_type,flags)
 	local flags = flags or 0
