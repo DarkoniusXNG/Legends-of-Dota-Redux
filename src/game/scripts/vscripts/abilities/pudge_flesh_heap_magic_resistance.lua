8e81e86cb731e730e81f5c687548860ec5fafbc4
Darko V <darko1290@gmail.com>
2026-05-17 01:16:07 +0200

    Some Flesh Heap cleanup
    All Flesh Heaps: Added undead perk tag.
    Flesh Heap Armor is now breakable.
    Flesh Heap Armor added tank tag.
    Flesh Heap Cooldown Reduction is now breakable.
    Flesh Heap Evasion is now breakable.
    Flesh Heap Health Regen added tank tag.
    Flesh Heap Mana Regen cannot be randomed anymore.
    Flesh Heap Movement Speed added mobility perk tag.
    Flesh Heap Magic Resistance is now breakable.
    Flesh Heap Vision cannot be randomed anymore.

--- src/game/scripts/vscripts/abilities/pudge_flesh_heap_magic_resistance.lua
+++ src/game/scripts/vscripts/abilities/pudge_flesh_heap_magic_resistance.lua
@@ -69,6 +69,9 @@ function modifier_flesh_heap_magic_resistance:DeclareFunctions()
 end
 
 function modifier_flesh_heap_magic_resistance:GetModifierMagicalResistanceBonus()
-  local parent = self:GetParent()
-  return math.min(math.floor(parent:GetModifierStackCount("modifier_pudge_custom_flesh_heap_kill_tracker", parent) * self.flesh_heap_amount), 100)
+	local parent = self:GetParent()
+	if parent:PassivesDisabled() then
+		return 0
+	end
+	return math.min(math.floor(parent:GetModifierStackCount("modifier_pudge_custom_flesh_heap_kill_tracker", parent) * self.flesh_heap_amount), 100)
 end
