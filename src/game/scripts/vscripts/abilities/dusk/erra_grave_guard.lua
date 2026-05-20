c5130fde409811f242472f85ec138172d074c5fb
Darko V <darko1290@gmail.com>
2026-05-20 15:41:24 +0200

    soul rip op
    New ability: Soul Rip OP
    Grave Guard cd no longer ignores cd reductions.
    Grave Guard mana cost no longer ignores mana cost reductions.

--- src/game/scripts/vscripts/abilities/dusk/erra_grave_guard.lua
+++ src/game/scripts/vscripts/abilities/dusk/erra_grave_guard.lua
@@ -3,94 +3,62 @@ erra_grave_guard = class({})
 LinkLuaModifier("modifier_grave_guard","abilities/dusk/erra_grave_guard",LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_grave_guard_recovery","abilities/dusk/erra_grave_guard",LUA_MODIFIER_MOTION_NONE)
 
-LinkLuaModifier("modifier_grave_guard_aura","abilities/dusk/erra_grave_guard",LUA_MODIFIER_MOTION_NONE)
-
 function erra_grave_guard:GetIntrinsicModifierName()
-
-	return "modifier_grave_guard_aura"
+	return "modifier_grave_guard"
 end
+
+function erra_grave_guard:ShouldUseResources()
+	return true
+end
+
+---------------------------------------------------------------------------------------------------
 
 modifier_grave_guard = class({})
 
+function modifier_grave_guard:IsHidden()
+	return true
+end
+
+function modifier_grave_guard:IsDebuff()
+	return false
+end
+
+function modifier_grave_guard:IsPurgable()
+	return false
+end
+
 function modifier_grave_guard:DeclareFunctions()
-	local funcs = {
+	return {
 		MODIFIER_EVENT_ON_TAKEDAMAGE
 	}
-	return funcs
 end
 
 if IsServer() then
-
 	function modifier_grave_guard:OnTakeDamage(params)
-		local threshold = self:GetAbility():GetSpecialValueFor("threshold")
-
 		local caster = self:GetParent()
+		local ability = self:GetAbility()
 
 		if params.unit ~= caster then return end
 
-		local hp = caster:GetHealthPercent()
-
-		local duration = self:GetAbility():GetSpecialValueFor("duration")
-
 		if not caster:IsRealHero() then return end
 
-		local cast_me = self:GetAbility():IsCooldownReady() and self:GetAbility():IsOwnersManaEnough()
+		local duration = ability:GetSpecialValueFor("duration")
+		local threshold = ability:GetSpecialValueFor("threshold")
+		local cast_me = ability:IsCooldownReady() and ability:IsOwnersManaEnough()
+		local hp = caster:GetHealthPercent()
 
-		local mana_cost = self:GetAbility():GetManaCost(self:GetAbility():GetLevel()-1)
+		if hp < threshold and cast_me and caster:IsAlive() then
+			caster:AddNewModifier(caster, ability, "modifier_grave_guard_recovery", {duration = duration})
 
-		local cooldown = self:GetAbility():GetCooldown(self:GetAbility():GetLevel()-1)
+			caster:EmitSound("Erra.GraveGuard")
 
-		if hp < threshold then
-
-			if cast_me then
-				local duration = self:GetAbility():GetSpecialValueFor("duration")
-
-				if mana_cost > 0 then
-					caster:SpendMana(mana_cost, self:GetAbility())
-				end
-
-				caster:AddNewModifier(caster, self:GetAbility(), "modifier_grave_guard_recovery", {Duration=duration}) --[[Returns:void
-				No Description Set
-				]]
-				caster:EmitSound("Erra.GraveGuard")
-
-				self:GetAbility():StartCooldown(cooldown)
-			end
-
+			ability:UseResources(true, false, false, true)
 		end
 	end
 
 end
 
-modifier_grave_guard_aura = class({})
-
-function modifier_grave_guard_aura:IsAura()
-	return true
-end
-
-function modifier_grave_guard_aura:GetAuraRadius()
-	return 1
-end
-
-function modifier_grave_guard_aura:GetAuraSearchFlags()
-	return 0
-end
-
-function modifier_grave_guard_aura:GetAuraSearchTeam()
-	return DOTA_UNIT_TARGET_TEAM_FRIENDLY
-end
-
-function modifier_grave_guard_aura:GetAuraSearchType()
-	return DOTA_UNIT_TARGET_HERO
-end
-
-function modifier_grave_guard_aura:GetModifierAura()
-	return "modifier_grave_guard"
-end
-
-function modifier_grave_guard_aura:IsHidden()
-	return true
-end
+---------------------------------------------------------------------------------------------------
 
 modifier_grave_guard_recovery = class({})
 
@@ -99,11 +67,10 @@ function modifier_grave_guard_recovery:GetEffectName()
 end
 
 function modifier_grave_guard_recovery:DeclareFunctions()
-	local funcs = {
+	return {
 		MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
 		MODIFIER_PROPERTY_MANA_REGEN_CONSTANT
 	}
-	return funcs
 end
 
 function modifier_grave_guard_recovery:GetModifierConstantHealthRegen()
