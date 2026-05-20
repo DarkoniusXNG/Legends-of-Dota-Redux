2fbbc2cef52f03ea3bd2d29f0864bc78536d82ee
Darko V <darko1290@gmail.com>
2026-05-20 22:52:05 +0200

    woops
    Glimpse OP: Removed mobility perk tag.
    Glimpse OP: Added stun tag.

--- src/game/scripts/vscripts/abilities/life_in_arena/devour.lua
+++ src/game/scripts/vscripts/abilities/life_in_arena/devour.lua
@@ -1,7 +1,7 @@
 function Devour(keys)
 	local target = keys.target
 	local instant_heal = keys.instant_heal
-	if string.find(target:GetUnitName(),"boss") or target:IsHero() then 
+	if target:IsRoshanCustom() or target:IsHero() then 
 	 	keys.ability:ApplyDataDrivenModifier(keys.caster, target, "modifier_ghoul_devour_slow", nil)		
 	else
 		keys.target:Kill(keys.ability, keys.caster)
